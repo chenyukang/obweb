d0cf0960eaa7c0f8d27408d401a4f6be08f0c329
yukang <moorekang@gmail.com>
2021-11-10 21:22:34 +0800

    fix update

--- bin/run.sh
+++ bin/run.sh
@@ -2,4 +2,4 @@ pushd ob
 git checkout main
 git pull
 popd
-cargo build --release; rm -rf ./ob-web; cp target/release/ob-web ./ ; nohup ./ob-web  >/tmp/nog 2>&1 &
+cargo build --release; rm -rf ./ob-web; cp target/release/ob-web ./ ; cp target/release/rss-reader ./; nohup ./ob-web  >/tmp/nog 2>&1 &
