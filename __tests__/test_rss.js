9283915056f263a6902581c64eb246b4a58e8faa
yukang <moorekang@gmail.com>
2022-04-22 20:41:09 +0800

    continue rss

--- __tests__/test_rss.js
+++ __tests__/test_rss.js
@@ -2,16 +2,23 @@
 const fs = require('fs')
 const RSS = require('../rss.js');
 
+const db_path = "/tmp/rss.db";
+
 
 describe('basic route tests', () => {
+
+    beforeEach(function() {
+        if (fs.existsSync(db_path)) {
+            fs.unlinkSync(db_path);
+        }
+    });
+
     test('rss db init', async() => {
-        let db_path = "/tmp/rss.db";
-        RSS.initRssDB(db_path);
+        RSS.rssDB(db_path);
         expect(fs.existsSync(db_path)).toBe(true);
     });
 
-    test('rss fetch and parse', () => {
-        RSS.fetchFeed('http://catcoding.me/atom.xml');
-
+    test('rss fetch and parse', async() => {
+        await RSS.fetchFeed('http://catcoding.me/atom.xml', db_path);
     });
 });
--- rss.js
+++ rss.js
@@ -1,7 +1,7 @@
 const sqlite = require('better-sqlite3');
-const { assert } = require('koa/lib/context');
 const fs = require('fs')
-const axios = require('axios')
+const path = require('path')
+let Parser = require('rss-parser');
 
 let initdb_sql = `
 CREATE TABLE IF NOT EXISTS pages(
@@ -14,29 +14,44 @@ CREATE TABLE IF NOT EXISTS pages(
     source String NOT NULL)
 `;
 
-function initRssDB(db_path) {
+function rssDB(db_path) {
     if (!fs.existsSync(db_path)) {
         const RSSDB = new sqlite(db_path);
         RSSDB.prepare(initdb_sql).run();
         RSSDB.prepare(`CREATE UNIQUE INDEX idx_pages_link ON pages(link)`).run();
     }
+    return new sqlite(db_path);
 }
 
-function fetchFeed(feed_url) {
-    axios
-        .get(feed_url)
-        .then(res => {
-            console.log(`statusCode: ${res.status}`)
-            console.log(res)
-            let body = res.data;
-            console.log(body);
-        })
-        .catch(error => {
-            console.error(error)
-        })
+function get_rss_page(link, db_path) {
+    return rssDB(db_path).prepare(`SELECT * FROM pages WHERE link = ?`).all(link);
+}
+
+async function fetchFeed(feed_url, db_path) {
+    let parser = new Parser();
+    let res = [];
+    let feed = await parser.parseURL(feed_url);
+    //console.log(feed.title);
+    feed.items.forEach(item => {
+        //console.log(item.title + ':' + item.link)
+        res.push(item);
+        let pre = get_rss_page(item.link, db_path);
+        if (pre.length == 0) {
+            let res = rssDB(db_path).prepare(
+                "INSERT INTO pages (title, link, website, publish_datetime, readed, source) values (?, ?, ?, ?, ?, ?)",
+            ).run(item.title, item.link, item.link, item.pubDate, 0, feed_url);
+            console.log(res);
+            let page = get_rss_page(item.link, db_path)[0];
+            let page_path = path.resolve(`./pages/${page.id}.html`);
+            console.log(page_path);
+            fs.writeFileSync(page_path, item.content);
+        }
+    });
+
+    return res;
 }
 
 module.exports = {
-    initRssDB,
+    rssDB,
     fetchFeed
 }
